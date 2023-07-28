aec4e78dbb42d07e8df11086101ff57f8781facf
Mac <mac@MacBook-Air-Mac.local>
2023-07-28 08:09:06 +0500

    name

--- create_one_attribute.py
+++ create_one_attribute.py
@@ -1,2 +1,9 @@
 #Create a "Person" class
 #Create an attribute "name" in the "Person" class
+class Person:
+    def __init__(self,name) -> None:
+        self.name = name
+p1=Person('Ma\'ruf')
+
+print(p1.name)
+        
